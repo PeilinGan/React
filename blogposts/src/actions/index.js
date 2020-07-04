d3ec1b30f873b223941f6a019af07a1c61d17cac
Peilin Gan <pgan@uci.edu>
2020-07-04 02:25:08 -0700

    finish blog posts with redux

--- blogposts/src/actions/index.js
+++ blogposts/src/actions/index.js
@@ -1,6 +1,17 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async (dispatch) => {
-  const response = await jsonPlaceholder.get("/posts");
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  const posts = await jsonPlaceholder.get("/posts");
+  dispatch({ type: "FETCH_POSTS", payload: posts.data });
 };
+
+export const fetchUser = (userId) => (dispatch) => {
+  _fetchUser(userId, dispatch);
+};
+
+//lodash one-time memorization to memorize the same api request
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+  const user = await jsonPlaceholder.get(`/users/${userId}`);
+  dispatch({ type: "FETCH_USER", payload: user.data });
+});
